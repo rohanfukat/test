a7b59ced0a01295e17d454f90dd6b7f9da7e6952
rohanfukat <rohanfukat123@gmail.com>
2025-03-30 00:20:23 +0530

    testing

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # specific frontend ka URL de sakte ho
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class userValidation(BaseModel):
+    username: str
+    email: str | None = None
+    password: str
+    tax: float | None = None
+
+@app.post("/user/")
+def demo(user:userValidation):
+    return {"Success":user}
+
+
